9c913d4c8bd2cb4f4983c8b169e0f980281c78ea
Sheiynee <rickarmand3@gmail.com>
2026-04-01 22:33:11 +0800

    read firebase credentials from env variable

--- server/db.js
+++ server/db.js
@@ -3,11 +3,16 @@ import { readFileSync } from 'fs'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+let serviceAccount
 
-const serviceAccount = JSON.parse(
-  readFileSync(join(__dirname, 'serviceAccountKey.json'), 'utf8')
-)
+if (process.env.GOOGLE_CREDENTIALS) {
+  serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS)
+} else {
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+  serviceAccount = JSON.parse(
+    readFileSync(join(__dirname, 'serviceAccountKey.json'), 'utf8')
+  )
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
